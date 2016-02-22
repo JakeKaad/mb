2b16e52ce44b119b9282a6c75c98a737f2376718
Jake Kaad <Kaadalac@gmail.com>
2016-02-21 19:01:16 -0800

    setup rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,50 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rails'
+require 'shoulda_matchers'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+
+require 'database_cleaner'
+
+Capybara.javascript_driver = :webkit
+
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.before(:each, type: :feature) do
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+    if !driver_shares_db_connection_with_specs
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
