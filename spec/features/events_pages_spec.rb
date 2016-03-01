97d9a3065d3ff37252a6156ecb0e2eb43d48638b
Jake Kaad <Kaadalac@gmail.com>
2016-02-29 18:11:00 -0800

    add failing test for information creation

--- spec/features/events_pages_spec.rb
+++ spec/features/events_pages_spec.rb
@@ -16,4 +16,25 @@ feature "Company and meta information" do
     info = event_with_info.info
     expect(page).to have_content info.linen_colors
   end
+
+  scenario "we can add new info" do
+    visit company_event_path company, event_without_info
+    fill_in_info_params
+    click_on "Create info"
+    expect(page).to have_content "Information added to event"
+  end
+end
+
+def fill_in_info_params info=nil
+  info ||= build :info
+  fill_in "Linen colors", with: info.linen_colors
+  fill_in "Napkin colors", with: info.napkin_colors
+  fill_in "Napkin fold", with: info.napkin_fold
+  fill_in "Placesetting", with: info.placesetting
+  fill_in "Centerpieces", with: info.centerpieces
+  fill_in "Water service", with: info.water_service
+  fill_in "Coffee service", with: info.coffee_service
+  fill_in "Final guest count", with: info.final_guest_count
+  fill_in "Number of tables", with: info.number_of_tables
+  fill_in "Guests per table", with: info.guests_per_table
 end
