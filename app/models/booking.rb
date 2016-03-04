ae470b194e5167c2f4eeb6212e046b0c4b425009
Jake Kaad <Kaadalac@gmail.com>
2016-03-04 08:34:31 -0800

    add validations to bookings table

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ActiveRecord::Base
   belongs_to :event
   belongs_to :company
   belongs_to :customer
+  validates_presence_of :event_id, :customer_id, :company_id
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -5,4 +5,8 @@ describe Booking do
   it { should belong_to :company }
   it { should belong_to(:customer) }
   it { should belong_to(:event) }
+
+  it { should validate_presence_of :event_id }
+  it { should validate_presence_of :company_id }
+  it { should validate_presence_of :customer_id }
 end
