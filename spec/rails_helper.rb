68b13f7e328d405a9b5cf4bc18c5672488a0f549
Jake Kaad <Kaadalac@gmail.com>
2016-03-06 09:43:24 -0800

    invoke DB from rails_helper for travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+if ENV['TRAVIS']
+  Rake::Task["db:create"].invoke
+  Rake::Task["db:schema:load"].invoke
+else
+  Rake::Task["db:environment"].invoke
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
