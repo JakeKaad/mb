8305b772eb6aad8180e8ad109257a8fdfb31e298
Jake Kaad <Kaadalac@gmail.com>
2016-03-05 21:37:53 -0800

    Some UI improvements and Seed file improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@ event_names = []
 10.times { event_names << "#{Faker::Name.first_name}'s School Auction" }
 
 
+
+
 user = User.first_or_create email: "test@test.com", password: "password", first_name: "Test", last_name: "User"
 company = Company.first_or_create name: "Melody Ballroom"
 
@@ -16,7 +18,16 @@ n = 1
 15.times do
   event = Event.create date: n.weeks.from_now, start_time: n.weeks.from_now, name: event_names.sample, room_id: company.rooms.sample.id
   company.events << event
-  event.primary_contact = Customer.create email: "test_#{n}@test.test", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "password"
+  event.primary_contact = Customer.create email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "password"
+  event.primary_contact.contact_cards.create(
+    phone: Faker::PhoneNumber.phone_number,
+    street_address: Faker::Address.street_address,
+    address_second_line: Faker::Address.secondary_address,
+    email: Faker::Internet.email,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip
+  )
   event.customers << event.primary_contact
   Booking.create event_id: event.id, customer_id: event.primary_contact.id, company_id: company.id
   n += 1
