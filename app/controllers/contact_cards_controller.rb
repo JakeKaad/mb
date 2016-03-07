5bd9b23c98a636d7c16cbb21f62355b6fbee4b98
Jake Kaad <Kaadalac@gmail.com>
2016-03-06 23:23:02 -0800

    * Add Async JS contact card creation * UI is janky * Need tests

--- app/controllers/contact_cards_controller.rb
+++ app/controllers/contact_cards_controller.rb
@@ -1,12 +1,39 @@
 class ContactCardsController < ApplicationController
+  before_action :set_customer
 
   def new
-    @customer = Customer.find params[:customer_id]
+    authorize! :manage, @customer
     @contact_card = ContactCard.new
 
     respond_to do |format|
       format.html { redirect_to root_path, alert: "Not supported yet." }
       format.js {}
     end
+  end
+
+  def create
+    authorize! :manage, @customer
+    @contact_card = @customer.contact_cards.new(contact_card_params)
+    if @contact_card.save
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: "Not supported yet." }
+        format.js {}
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: "Not supported yet." }
+        format.js { render :new }
+      end
+    end
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find params[:customer_id]
+  end
+
+  def contact_card_params
+    params.require(:contact_card).permit(:phone, :street_address, :address_second_line, :city, :state, :zip, :email)
   end
 end
