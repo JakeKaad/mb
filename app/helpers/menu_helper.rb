7cc3d79a7ede9e5695c9961f74369113a39070ce
Jake Kaad <Kaadalac@gmail.com>
2016-04-04 08:27:31 -0700

    move menu option select creation to helper method

---
+++ app/helpers/menu_helper.rb
@@ -0,0 +1,17 @@
+module MenuHelper
+  def menu_option_helper form
+    form.select :menu_option_id,
+      options_for_select(map_menu_options, include_blank: "Menu Option")
+  end
+
+
+  def map_menu_options
+    MenuOption.all.map do |mo|
+       [mo.name,
+        mo.id, {
+          'data-price' => mo.suggested_price_adjustment,
+          'data-course' => mo.suggested_course
+        }]
+    end + [["Add a new one", "new"]]
+  end
+end
