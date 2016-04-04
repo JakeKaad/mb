05911af95bd411d603871aa547ab200115b40923
Jake Kaad <Kaadalac@gmail.com>
2016-04-04 08:37:46 -0700

    finish JS

--- app/assets/javascripts/menu_options.js
+++ app/assets/javascripts/menu_options.js
@@ -5,6 +5,9 @@ $(function() {
       $("#menu_item_course_group").hide();
       $("#menu_item_price_adjustment_group").hide();
     } else {
+      $("#options_form_hidden").css("display", "none");
+      $("#menu_item_course_group").show();
+      $("#menu_item_price_adjustment_group").show();
       $("#menu_item_course").val(this.selectedOptions[0].dataset.course);
       $("#menu_item_price_adjustment").val(this.selectedOptions[0].dataset.price);
     }
