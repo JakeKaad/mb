8fa330d9590a6738b67c6096553b03ff91b673b1
Jake Kaad <Kaadalac@gmail.com>
2016-04-04 08:36:55 -0700

    Finish JS for form

--- app/assets/javascripts/menu_options.js
+++ app/assets/javascripts/menu_options.js
@@ -1,6 +1,12 @@
 $(function() {
   $("#menu_item_menu_option_id").change(function() {
-    $("#menu_item_course").val(this.selectedOptions[0].dataset.course);
-    $("#menu_item_price_adjustment").val(this.selectedOptions[0].dataset.price);
+    if(this.value === "new") {
+      $("#options_form_hidden").css("display", "block");
+      $("#menu_item_course_group").hide();
+      $("#menu_item_price_adjustment_group").hide();
+    } else {
+      $("#menu_item_course").val(this.selectedOptions[0].dataset.course);
+      $("#menu_item_price_adjustment").val(this.selectedOptions[0].dataset.price);
+    }
   });
 });
